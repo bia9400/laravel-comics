5fe97f17fccbf5665112bb94704374dbed4aec31
bia9400 <ferro.biagio@gmail.com>
2022-07-26 17:46:43 +0200

    end first day

--- resources/views/partials/card.blade.php
+++ resources/views/partials/card.blade.php
@@ -1,6 +1,6 @@
 <div class="mycard">
     <div class="card-image-squared">
-      <img :src="{{$fumetto["thumb"]}}" alt="" />
+      <img src="{{$fumetto["thumb"]}}" alt="" />
     </div>
     <div class="title-of-img py-3">
       <h6>{{$fumetto["title"]}}</h6>
